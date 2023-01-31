0342bd87ac3403ce377d824747c8bf1d46b37d29
Toni Hirvonen <tovialhi@student.jyu.fi>
2023-01-31 21:22:44 +0200

    t3.11

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const cors = require('cors')
 
 const app = express()
 app.use(express.json())
+app.use(express.static('build'))
 app.use(cors())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content', 'skip'))
 
@@ -88,7 +89,7 @@ app.get('/', (req, res) => {
     res.send('<h1>Hello World!</h1>')
     })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
 console.log(`Server running on port ${PORT}`)
 })

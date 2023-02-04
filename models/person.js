2f7c17b863f70079aa25cf8c649485c4bd103cc1
Toni Hirvonen <tovialhi@student.jyu.fi>
2023-02-04 02:23:03 +0200

    t3.20

--- models/person.js
+++ models/person.js
@@ -18,12 +18,20 @@ mongoose.connect(url)
     name: {
       type: String,
       minlength: 3,
-      required: true
+      required: [true, 'Name required']
     },
 
     number: {
       type: String,
-      required: true
+      minlength: 8,
+
+      validate: {
+        validator: (v) => {
+          return /\d{3}-\d{5}/.test(v)
+        },
+        message: props => `${props.value} is not a valid phone number!`
+      },
+      required: [true, 'Number required']
     }
 })
 
